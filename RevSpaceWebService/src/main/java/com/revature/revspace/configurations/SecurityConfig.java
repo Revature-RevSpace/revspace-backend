0d63483bf5263d89b7c38af605e1d3de012d4bbf
Joseph B <josephbettendorff@yahoo.com>
2021-11-17 16:03:23 -0600

    Add Security Config

---
+++ RevSpaceWebService/src/main/java/com/revature/revspace/configurations/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.revature.revspace.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig
+{
+
+}
