24b8743798f14e369403717d908d7cc4201186d0
tlatham54 <tlatham54@gmail.com>
2021-11-17 17:14:55 -0500

    Getters and setters for the Like model.

--- RevSpaceWebService/src/main/java/com/revature/revspace/models/Likes.java
+++ RevSpaceWebService/src/main/java/com/revature/revspace/models/Likes.java
@@ -1,6 +1,7 @@
 package com.revature.revspace.models;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="likes")
@@ -19,7 +20,63 @@ public class Likes {
     @JoinColumn(name = "post_id")
     private Post postId;
 
+    public Likes() {
+    }
 
+    public Likes(User userId, Post postId) {
+        this.userId = userId;
+        this.postId = postId;
+    }
 
+    public Likes(int likeId, User userId, Post postId) {
+        this.likeId = likeId;
+        this.userId = userId;
+        this.postId = postId;
+    }
 
+    public int getLikeId() {
+        return likeId;
+    }
+
+    public void setLikeId(int likeId) {
+        this.likeId = likeId;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Post getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Post postId) {
+        this.postId = postId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Likes)) return false;
+        Likes likes = (Likes) o;
+        return getLikeId() == likes.getLikeId() && Objects.equals(getUserId(), likes.getUserId()) && Objects.equals(getPostId(), likes.getPostId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLikeId(), getUserId(), getPostId());
+    }
+
+    @Override
+    public String toString() {
+        return "Likes{" +
+                "likeId=" + likeId +
+                ", userId=" + userId +
+                ", postId=" + postId +
+                '}';
+    }
 }
