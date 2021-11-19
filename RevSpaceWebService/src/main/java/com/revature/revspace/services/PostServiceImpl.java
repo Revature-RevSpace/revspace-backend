a6de70e3897d0f993ba355e8ac9991c748e5a243
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-19 16:27:32 -0600

    add some codes on backend PostService

--- RevSpaceWebService/src/main/java/com/revature/revspace/services/PostServiceImpl.java
+++ RevSpaceWebService/src/main/java/com/revature/revspace/services/PostServiceImpl.java
@@ -15,9 +15,8 @@ public class PostServiceImpl implements PostService{
     PostRepo postRepo;
 
     //The larger the unix time, the newer the post.
-
-    static List<Post> sortedCurrentPostsList = new ArrayList<>();       // Posts are in descending order
-    static List<Post> sortedCurrentCommentsList = new ArrayList<>();   // Comments are in ascending order
+//    static List<Post> sortedCurrentPostsList = new ArrayList<>();       // Posts are in descending order
+//    static List<Post> sortedCurrentCommentsList = new ArrayList<>();   // Comments are in ascending order
 
 
     @Override
@@ -30,6 +29,73 @@ public class PostServiceImpl implements PostService{
         return null;
     }
 
+    public void pullPostsList(int lastPostIdOnThePage, int firstPostIdOnThePage){
+
+        List<Post> sortedCurrentPostsList = postRepo.findByCommentFalseOrderByDateDesc();
+        List<Post> sortedCurrentCommentsList = postRepo.findByCommentTrueOrderByDateAsc();
+        List<Post> resposePostsList = new ArrayList<>();
+        List<Post> resposeCommentsList = new ArrayList<>();
+
+        if(lastPostIdOnThePage == 0) {
+            resposePostsList = sortedCurrentPostsList.subList(0, 9);
+            for (Post post : resposePostsList) {
+                resposeCommentsList.addAll(
+                        selectedRelatedComments(post, sortedCurrentCommentsList));
+            }
+        }
 
 
+    }
+
+//    public void updatePostsList(boolean comment){
+//        if(comment){
+//            int biggestCommentIdJava = sortedCurrentCommentsList.get(sortedCurrentCommentsList.size() - 1).getPostId();
+//            int biggestCommentIdDatabase = postRepo.findFirstByCommentTrueOrderByPostIdAsc();
+//            if (biggestCommentIdJava != biggestCommentIdDatabase){
+//                sortedCurrentCommentsList.addAll(postRepo.findByCommentTrueAndPostIdGreaterThanOrderByDateAsc(biggestCommentIdJava));
+//            }
+//        }else {
+//            int biggestPostIdJava = sortedCurrentPostsList.get(sortedCurrentPostsList.size() - 1).getPostId();
+//
+//            if (biggestPostIdJava != biggestCommentIdDatabase){
+//                sortedCurrentPostsList.addAll(0, postRepo.findByCommentFalseAndPostIdGreaterThanOrderByDateDesc(biggestPostIdJava));
+//            }
+//        }
+//    }
+
+//    public List<Post> selectedRelatedComments (int parentsPostId, List<Post> allComments){
+//    public List<Post> selectedRelatedComments (Post parentsPost, List<Post> allComments){
+//        List<Post> childrenComments = new ArrayList<>();
+//        for (Post comment : allComments) {
+//            if (parentsPost == comment.getParentPost()) {
+//                childrenComments.add(comment);
+//            }
+//        }
+//        if (childrenComments.size() == 0) { return null; }
+//        else {
+//            for (Post comment : childrenComments) {
+//                 selectedRelatedComments(comment, allComments);
+//            }
+//        }
+//        return parentsList.addAll(childrenComments);
+//    }
+
+
+
+
+
+    public List<Post> selectedRelatedComments (Post parentsPost, List<Post> allComments){
+        List<Post> childrenComments = new ArrayList<>();
+        for (Post comment : allComments) {
+            if (parentsPost == comment.getParentPost()) {
+                childrenComments.add(comment);
+            }
+        }
+        List<Post> childrenOfChildren = new ArrayList<>();
+        for(Post parentsComment : childrenComments){
+            childrenOfChildren.addAll(selectedRelatedComments(parentsComment, allComments));
+        }
+        childrenComments.addAll(childrenOfChildren);
+        return childrenComments;
+    }
 }
