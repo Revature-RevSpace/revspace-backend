472197d8c00eba9570c06b980488bcd394a586bf
Joseph B <josephbettendorff@yahoo.com>
2021-11-30 14:08:29 -0600

    Attempt to fix too many postgres connections

--- RevSpaceWebService/src/test/java/com/revature/revspace/repositories/UserRepoTests.java
+++ RevSpaceWebService/src/test/java/com/revature/revspace/repositories/UserRepoTests.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest(classes= RevSpaceWebServiceApplication.class)
 @Transactional
 public class UserRepoTests
